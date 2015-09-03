5466cb0d50a5b72836bf3f25460e4f59494874e4
Elache <lohibon@free.fr>
2015-09-03 15:09:01 +0200

    Version 03.09.2015 Fonctionnelle - (pseudo pré-rempli)

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<?php
-if(!isset($_GET['qui_posteur']))
-{
-		$retiens_posteur = "Votre pseudo";
-		echo $retiens_posteur;
-		setcookie('cookie_chat',$retiens_posteur,time()+3600,null,null,false,true);
-}
-{		
-		$retiens_posteur = strip_tags($_GET['qui_posteur']);
-		echo $retiens_posteur;
-		setcookie('cookie_chat',$retiens_posteur,time()+3600,null,null,false,true);
-}
-
-// cookie, stockée 1 heure, httpOnly activé pour sécurité
-
-
-
-?>
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
    <head>
@@ -41,26 +21,41 @@ if(!isset($_GET['qui_posteur']))
  }
 
  
+if(isset($_GET['qui_posteur']))
+	{
+		$retiens_posteur = strip_tags($_GET['qui_posteur']);
+	} else
+	{		
+		$retiens_posteur = "Votre pseudo";
+	}
+
  
  ?>
 
 <!-- Formulaire -->
 <form method="post" action="minichat_post.php">
-	<P>Votre pseudo : <input type="text" name="posteur" value="<?php echo $_COOKIE['cookie_chat'] ; ?>"><br/>
+	<P>Votre pseudo : <input type="text" name="posteur" value="<?php echo $retiens_posteur ; ?>"><br/>
 	<P>Votre message : <input type="text" name="message" value="Message"><br/>
 	<input type="submit" submit="Envoyer">
 	<!-- // récupération du nom du posteur à implémenter (sur cookie) et en value pour posteur -->
 </form>
 
+
 <?php
 
 $reponse=$bdd->query('SELECT message,posteur,date_message FROM messages ORDER BY date_message DESC LIMIT 0,5');
+
+
+	
+	
+
 
 while($donnees=$reponse->fetch())
 {
 	// Suppression des tags HTML // Sécurité-XSS
 	$posteur_html = strip_tags($donnees['posteur']);
 	$message_html = strip_tags($donnees['message']);
+
 	?>
 
 	<table border=1 bordercolor=navy>
@@ -85,12 +80,22 @@ while($donnees=$reponse->fetch())
 
 <?php
 }	
+?>
 
-/*
-echo  '<P>Messages précédents <a href=>page 2</A> ; <a href=>page 3</A> ; <a href=>page 4</A> ; <a href=>page 5</A></P>';
-5-5 10-5 15-5 21et+
-*/
+<table border=1 rules=rows>
+	<TR>
+		<TD bgcolor=#efefef colspan=4 align=center>Messages pr&eacute;c&eacute;dents</TD>
+	</TR>
+	<TR>
+		<TD width=60 align=center><a href=javascript:void(0)>6 - 10</A></TD>
+		<TD width=60 align=center><a href=javascript:void(0)>11 - 15</A></TD>
+		<TD width=60 align=center><a href=javascript:void(0)>16 - 20</A></TD>
+		<TD width=120 align=center><a href=javascript:void(0)>21 et suivants</A></TD>
+	</TR>
+</table>
+<!-- sql : LIMIT 5-5 10-5 15-5 20-reste -->
 
+<?php
 $reponse -> closeCursor();
 
 ?>
